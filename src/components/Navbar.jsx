dff41eeef4418158d7fec4414fdd5cad5632537b
punit <outlanderskyo@gmail.com>
2025-05-31 22:46:26 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -168,7 +168,7 @@ const Navbar = () => {
         }`}
         style={{
           backgroundColor: '#F5EFFF',
-          backgroundImage: 'linear-gradient(to left, rgba(253, 159, 91, 1) 0%, rgba(125, 143, 247, 0.8) 10%, rgba(157, 92, 212, 0.6) 25%, rgba(248, 242, 255, 0.4) 100%)',
+          backgroundImage: 'linear-gradient(to left, rgba(253, 159, 91, 1) 0%, rgba(253, 159, 91, 1) 1%, rgba(125, 143, 247, 0.8) 6%, rgba(157, 92, 212, 0.6) 15%, rgba(248, 242, 255, 0.2) 35%)',
           height: `${windowHeight}px`, // Use dynamic height
           overflow: 'hidden',
           display: 'flex',
