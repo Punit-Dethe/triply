b55c478fea3bac8a5235cf695d14e29a229e82a6
punit <punitdethe2006@gmail.com>
2025-07-03 23:06:09 +0530

    Update Amenities.jsx

--- src/components/Amenities.jsx
+++ src/components/Amenities.jsx
@@ -111,9 +111,13 @@ const Amenities = () => {
             {/* VIP Services Section */}
             <div className="bg-white/30 backdrop-blur rounded-xl p-8 flex flex-col md:flex-row justify-between items-center border border-gray-200">
               <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6 md:mb-0 whitespace-nowrap">Check us out on Play Store</h2>
-              <button className="bg-black text-white px-8 py-3 rounded-full font-medium hover:bg-neutral-800 transition-colors border border-yellow-400">
+              <a 
+                href="https://play.google.com/store/apps/details?id=com.triply.app&pcampaignid=web_share"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-black text-white px-8 py-3 rounded-full font-medium hover:bg-neutral-800 transition-colors border border-yellow-400">
                 Download
-              </button>
+              </a>
             </div>
           </div>
         </div>
